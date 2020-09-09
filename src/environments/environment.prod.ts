07602f447802b24075555e57e88f844bce02a3c8
darreniyer0601 <54214698+darreniyer0601@users.noreply.github.com>
2020-09-09 18:31:26 -0400

    First Commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBYTJou2sCi42h7Z8IOs1VDhQ4SNfkiSx4',
+    authDomain: 'clientpanelprod-5a538.firebaseapp.com',
+    databaseURL: 'https://clientpanelprod-5a538.firebaseio.com',
+    projectId: 'clientpanelprod-5a538',
+    storageBucket: 'clientpanelprod-5a538.appspot.com',
+    messagingSenderId: '383394441110',
+    appId: '1:383394441110:web:5c3fe8d08071ecef089fb7',
+    measurementId: 'G-6G63YJ0D2N',
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBYTJou2sCi42h7Z8IOs1VDhQ4SNfkiSx4',
+    authDomain: 'clientpanelprod-5a538.firebaseapp.com',
+    databaseURL: 'https://clientpanelprod-5a538.firebaseio.com',
+    projectId: 'clientpanelprod-5a538',
+    storageBucket: 'clientpanelprod-5a538.appspot.com',
+    messagingSenderId: '383394441110',
+    appId: '1:383394441110:web:5c3fe8d08071ecef089fb7',
+    measurementId: 'G-6G63YJ0D2N',
+  },
 };
 
 /*
